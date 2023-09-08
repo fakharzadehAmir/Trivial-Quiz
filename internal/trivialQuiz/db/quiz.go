81f7f0513e09d8f443111c5b8a58743cce9cab70
Amir Fakharzadeh <amirfakharzadeh2002@gmail.com>
2023-09-08 12:22:48 +0330

    feat: add creating new quiz query using mongo driver

--- internal/trivialQuiz/db/quiz.go
+++ internal/trivialQuiz/db/quiz.go
@@ -2,26 +2,57 @@ package db
 
 import (
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
 type Quiz struct {
 	ID           primitive.ObjectID `bson:"_id,omitempty"`
-	QuizName     string             `bson:"quiz_name"`
 	Difficulty   string             `bson:"difficulty"`
-	Questions    primitive.A        `bson:"questions"`
+	Questions    []Question         `bson:"questions"`
 	UserAnswerer string             `bson:"user_answerer"`
-	Score        float64            `bson:"score"`
-	Category     string             `bson:"category"`
+	//	The Score is out of 10
+	Score      float64   `bson:"score"`
+	Category   string    `bson:"category"`
+	AnsweredAt time.Time `bson:"answered_at"`
+	CreatedAt  time.Time `bson:"created_at"`
 }
 
-func (mdb *MongoDB) CreateNewQuiz(ctx *context.Context, newQuiz *Quiz) error {
+func (mdb *MongoDB) CreateNewQuiz(ctx *context.Context, newQuiz *Quiz) (interface{}, error) {
 	//	Proceed to create new quiz
-	_, err := mdb.Collections.
+	ID, err := mdb.Collections.
 		QuizCollection.Collection.InsertOne(*ctx, newQuiz)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	//	Quiz created successfully
+	return ID, nil
+}
+
+func (mdb *MongoDB) UpdateScoreByID(ctx *context.Context,
+	quizId primitive.ObjectID, answers []string) error {
+	//	Check existence of quiz with given id
+	findQuiz := mdb.Collections.
+		QuestionCollection.Collection.
+		FindOne(*ctx, bson.M{"_id": quizId})
+
+	//	Handle error of retrieving the question with given username
+	if findQuiz.Err() != nil {
+		return findQuiz.Err()
+	}
+
+	//	Decode existed question to the declared variable existedQuiz
+	existedQuiz := &Quiz{}
+	if err := findQuiz.Decode(existedQuiz); err != nil {
+		return err
+	}
+
+	//var checkScore float64
+	//for idx, value := range answers {
+	//	question, err := mdb.GetQuestionByID(ctx, existedQuiz.Questions[idx - 1])
+	//}
+
 	return nil
+
 }
